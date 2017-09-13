793f63665b541e49829615df9bd9364560f37369
Li Chen <lchen@uber.com>
2017-09-13 07:51:12 -0700

    added virtualenv set in cron sh, added remote setting in set_env

--- set_env.sh
+++ set_env.sh
@@ -1,2 +1,5 @@
+# for local computer
 export STRATEGY_PATH=/Users/lchen/Desktop/finance/strategy_platform/
+# for remote
+# export STRATEGY_PATH=/home/bigmii/strategy_platform/
 export PYTHONPATH=$PYTHONPATH:STRATEGY_PATH
--- trigger_cron.sh
+++ trigger_cron.sh
@@ -2,4 +2,5 @@
 export STRATEGY_PATH=/home/bigmii/strategy_platform/
 export PYTHONPATH=$PYTHONPATH:STRATEGY_PATH
 cd /home/bigmii/strategy_platform/
+. env/bin/activate
 python workflows/test_dow30.py
