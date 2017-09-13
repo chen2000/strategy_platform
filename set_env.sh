89f22c849808f5de6eee02df19cd7113294312b6
chen2000 <bigmii@bigmii2.arvixevps.com>
2017-09-13 08:00:40 -0700

    set remotely about trigger run log and sys env

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,5 @@
 # for local computer
-export STRATEGY_PATH=/Users/lchen/Desktop/finance/strategy_platform/
+# export STRATEGY_PATH=/Users/lchen/Desktop/finance/strategy_platform/
 # for remote
-# export STRATEGY_PATH=/home/bigmii/strategy_platform/
+export STRATEGY_PATH=/home/bigmii/strategy_platform/
 export PYTHONPATH=$PYTHONPATH:STRATEGY_PATH
