bc786dc1b5df1da3539d383b910027f7199651cb
chen2000 <bigmii@bigmii2.arvixevps.com>
2017-09-25 12:28:55 -0700

    for unknown reason, ignored .sh still in git

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 # for local computer
-export STRATEGY_PATH=/Users/lchen/Desktop/finance/strategy_platform/
+# export STRATEGY_PATH=/Users/lchen/Desktop/finance/strategy_platform/
 
 # for remote
-# export STRATEGY_PATH=/home/bigmii/strategy_platform/
+export STRATEGY_PATH=/home/bigmii/strategy_platform/
 
 # for the case single terminal, have existing python paths
 # export PYTHONPATH=$PYTHONPATH:$STRATEGY_PATH
